db3bafd7a730573276d1a274e6265ae7b8e2fe3f
Max Wang <maxwang@Maxs-MacBook-Pro-2.local>
2023-01-04 01:15:54 -0800

    added some dataloader

--- a2/linear_regression.py
+++ a2/linear_regression.py
@@ -17,7 +17,7 @@ class LinearRegressionModel(nn.Module):
     def __init__(self, num_param):
         ## TODO 1: Set up network
         super(LinearRegressionModel, self).__init__()
-        self.linear = torch.nn.linear(num_param, num_param) #what is number of outputs? 1
+        self.linear = torch.nn.linear(num_param, 1) #what is number of outputs? 1
         #pass
         
 
@@ -86,7 +86,7 @@ def mse_loss(output, target):
 
     loss = nn.mse_loss()
     return loss(output, target) # ... :>? 
-    pass
+    #pass
 
 
 def mae_loss(output, target):
@@ -116,54 +116,98 @@ def mae_loss(output, target):
     loss = torch.nn.l1
     return loss(output, target) # ... :< 
 
-    pass
+    #pass
 
 
 if __name__ == "__main__":
     ## Here you will want to create the relevant dataloaders for the csv files for which 
     ## you think you should use Linear Regression. The syntax for doing this is something like:
     # Eg:
-    # train_loader, val_loader, test_loader =\
-    #    get_data_loaders(path_to_csv, 
-    #                     transform_fn=data_transform  # Can also pass in None here
-    #                     train_val_test=[YOUR TRAIN/VAL/TEST SPLIT], 
-    #                     batch_size=YOUR BATCH SIZE)
+    train_loader, val_loader, test_loader = get_data_loaders(DS1.csv, #WHAT DATA TO TRAIN ON 
+                        transform_fn=data_transform  # Can also pass in None here
+    )
+                        # train_val_test=[TRAIN/VAL/TEST SPLIT], 
+                        # batch_size=YOUR BATCH SIZE)
 
     ## Now you will want to initialise your Linear Regression model, using something like
     # Eg:
-    # model = LinearRegressionModel(...)
+    model = LinearRegressionModel(2)
 
     ## Then, you will want to define your optimizer (the thing that updates your model weights)
     # Eg:
-    # optimizer = optim.[one of PyTorch's optimizers](model.parameters(), lr=0.01)
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
 
     ## Now, you can start your training loop:
     # Eg:
-    # model.train()
-    # for t in range(TOTAL_TIME_STEPS):
-    #   for batch_index, (input_t, y) in enumerate(train_loader):
-    #     optimizer.zero_grad()
-    #
-    #     preds = Feed the input to the model
-    #
-    #     loss = loss_fn(preds, y)  # You might have to change the shape of things here.
-    #     
-    #     loss.backward() 
-    #     optimizer.step()
-    #     
+    model.train()
+    #define total time steps
+    TOTAL_TIME_STEPS = 1 #increase if runs 
+    for t in range(TOTAL_TIME_STEPS):
+      for batch_index, (input_t, y) in enumerate(train_loader):
+        optimizer.zero_grad()
+    
+        preds = model(input)
+          #Feed the input to the model
+    
+        loss = loss_fn(preds, y)  # You might have to change the shape of things here.
+        
+        loss.backward() 
+        optimizer.step()
+        
     ## Don't worry about loss.backward() for now. Think of it as calculating gradients.
 
     ## And voila, your model is trained. Now, use something similar to run your model on
     ## the validation and test data loaders:
     # Eg: 
-    # model.eval()
-    # for batch_index, (input_t, y) in enumerate(val/test_loader):
-    #
-    #   preds = Feed the input to the model
-    #
-    #   loss = loss_fn(preds, y)
+    model.eval()
+    #elias: do twice for val and test
+    for batch_index, (input_t, y) in enumerate(test_loader):
+    
+      preds = model(input)
+    
+      loss = loss_fn(preds, y)
+
+    for batch_index, (input_t, y) in enumerate(val_loader):
+    
+      preds = model(input)
+    
+      loss = loss_fn(preds, y)
     #
     ## You don't need to do loss.backward() or optimizer.step() here since you are no
     ## longer training.
 
-    pass
+    #pass
+
+def plot_data2(w1, w2, b):
+    data2 = pd.read_csv('data/DS2.csv')
+    data2.columns = ['x', 'y']
+    plt.scatter(data2['x'],data2['y']) 
+    x = np.arange(-6, 6)
+    plt.plot(x, w1*x*x*x + w2*x +b, linestyle='-', c = 'r')
+
+    plt.title('Features2')
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    plt.show()
+
+def plot_data1(w1, w2, b):
+    data1 = pd.read_csv('data/DS1.csv')
+    data1.columns = ['x1','x2','y']
+    fig = plt.figure()
+    ax = fig.add_subplot(111,projection='3d')
+
+    ax.scatter(data1['x1'],data1['x2'],data1['y'])
+    x = np.linspace(-60,60)
+    y = np.linspace(-50,90)
+
+    X,Y = np.meshgrid(x,y)
+    Z=w1*X + w2*Y + b
+    surf = ax.plot_surface(X, Y, Z)
+
+    ax.set_title('The Features')
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.set_zlabel('y')
+
+    plt.show()
